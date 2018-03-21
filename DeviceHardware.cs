d9bc855b7c086903751fe9266efc40eac213bf00
Danny Cabrera <DannyC@Daniels-MBP.localdomain>
2018-03-20 21:56:09 -0400

    Initial commit
    Initial commit

---
+++ DeviceHardware.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GetDroidModel
+{
+    class Droid 
+    {
+        public string Manufacturer { get; set; }
+        public string MarketingName { get; set; }
+        public string Device { get; set; }
+        public string Model { get; set; }
+    }
+
+    public static class DeviceHardware
+    {
+        /// <summary>
+        /// Reads the device file assets file.
+        /// </summary>
+        static IEnumerable<string> ReadDeviceFile()
+        {
+            string line;
+            using (var reader = new StreamReader(Android.App.Application.Context.Assets.Open("supported_devices.csv")))
+            {
+                while ((line = reader.ReadLine()) != null)
+                    yield return line;
+            }
+        }
+
+        /// <summary>
+        /// Parses the line.
+        /// </summary>
+        /// <param name="line">Devices.csv file line.</param>
+        static Droid ParseLine(string line)
+        {
+            //Retail Branding, Marketing Name,Device,Model
+            var parms = line.Split(',');
+
+            return new Droid() { Manufacturer=parms[0], MarketingName=parms[1], Device=parms[2], Model=parms[3]};
+        }
+
+        /// <summary>
+        /// Gets the Android model.
+        /// </summary>
+        /// <param name="defaultValue">Default value.</param>
+        public static string GetModel(string defaultValue = "Unknown", bool includeManufacturer = false)
+        {
+            var droidDevice = (from line in ReadDeviceFile()
+                   let droid = ParseLine(line)
+                   where droid.Model == Android.OS.Build.Model || droid.Device == Android.OS.Build.Model
+                   select droid).FirstOrDefault();
+
+            if (droidDevice == null)
+                return defaultValue;
+            else
+            {
+                var deviceName = droidDevice.MarketingName == "" ? droidDevice.Model : droidDevice.MarketingName;
+
+                if (includeManufacturer && deviceName.StartsWith(droidDevice.Manufacturer, System.StringComparison.OrdinalIgnoreCase))
+                    return $"{droidDevice.Manufacturer} {deviceName}";
+                
+                return deviceName;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+GetDroidModel
+=============
+
+Simple Xamarin.Android library to return the device model.
+*******
+Will keep device list updated with devices that work with Google Play. At current moment 3/20/2018 list contains 18,554 devices.
+
+Updates:<br/>
+03/19/2018 - Created<br/>
+*******
+
+## Install
+
+#### [NuGet Gallery](https://www.nuget.org/packages/dannycabrera.GetDroidModel)
+```
+Install-Package dannycabrera.GetDroidModel
+```
+
+Sample
+-------
+
+```
+// Example for model SM-G960F
+Console.WriteLine(GetDroidModel.DeviceHardware.Model());
+```
+
+Result: "Galaxy S9"
+
+
+```
+// Return name including manufacturer
+Console.WriteLine(GetDroidModel.DeviceHardware.Model(defaultValue: "Unknown", includeManufacturer: true));
+```
+
+Result: "Samsung Galaxy S9"
+
+
+```
+// If device is not listed:
+Console.WriteLine(GetDroidModel.DeviceHardware.Model(defaultValue: "Unknown"));
+```
+
+Result: "Unknown"
